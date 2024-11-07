8168578698dbfc39659dc9f56e53429a656f3135
pintere <pintere@edu.htl-villach.at>
2024-11-07 12:51:23 +0100

    Updated challenges

--- flagfrenzy-sveltekit/src/routes/challenges/+page.svelte
+++ flagfrenzy-sveltekit/src/routes/challenges/+page.svelte
@@ -2,48 +2,65 @@
   import { onMount } from 'svelte';
   import { goto } from '$app/navigation';
 
-  let challenges = [];
+  let challengesByCategory = {}; // Object to hold challenges grouped by category
   let error = null;
 
   async function loadChallenges() {
+    try {
       const response = await fetch('/api/challenges');
-      challenges = await response.json();
+      if (!response.ok) throw new Error("Failed to load challenges");
 
+      // Set the categorized response directly to challengesByCategory
+      challengesByCategory = await response.json();
+    } catch (err) {
+      error = err.message;
+    }
   }
 
   onMount(loadChallenges);
-
 </script>
-  
-  <div>
+
+<div>
   <p>Challenges</p>
-  </div>
-  <div>
-    <h1>Challenges List</h1>
-    <table class="styled-table">
+</div>
+
+<div>
+  <h1>Challenges List</h1>
+  {#if error}
+    <p class="error">{error}</p>
+  {:else}
+    {#each Object.keys(challengesByCategory) as category}
+      <h2>{category}</h2>
+      <table class="styled-table">
         <thead>
-            <tr>
-                <th>Name</th>
-                <th>Category</th>
-                <th>Difficulty</th>
-                <th>Description</th>
-            </tr>
+          <tr>
+            <th>Name</th>
+            <th>Category</th>
+            <th>Difficulty</th>
+            <th>Description</th>
+          </tr>
         </thead>
         <tbody>
-            {#each challenges as challenge}
-                <tr>
-                    <td>{challenge.ChallengeName}</td> 
-                    <td>{challenge.Categorie}</td> 
-                    <td>{challenge.Difficulty}</td> 
-                    <td>{challenge.Description}</td> 
-                   
-                </tr>
-            {/each}
+          {#each challengesByCategory[category] as challenge}
+            <tr>
+              <td>{challenge.ChallengeName}</td>
+              <td>{challenge.Categorie}</td>
+              <td>{challenge.Difficulty}</td>
+              <td>{challenge.Description}</td>
+            </tr>
+          {/each}
         </tbody>
-    </table>
+      </table>
+    {/each}
+  {/if}
 </div>
-  <style>
-    h2 {
-        text-align: center;
-    }
-  </style>
+
+<style>
+  h2 {
+    text-align: center;
+  }
+  .error {
+    color: red;
+    text-align: center;
+  }
+</style>
