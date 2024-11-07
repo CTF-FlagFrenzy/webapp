ecd1bc160158c615ccb14392572482958da94878
Leon Attila Sturm <attila@Mac.htl-vil.local>
2024-11-07 10:46:22 +0100

    layout/header

---
+++ flagfrenzy-sveltekit/src/routes/+layout.svelte
@@ -0,0 +1,51 @@
+<script>
+	import Logo from '$lib/images/logo.png';
+</script>
+
+<link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet"/>
+
+<header>
+    <img alt="The project logo" src={Logo} />
+    <nav>
+        <a href="/challenges">Challenges</a>
+        <a href="/scoreboard">Scoreboard</a>
+        <a href="/team">Team</a>
+    </nav>
+    <button>
+        Login
+    </button>
+</header>
+
+<style>
+    header {
+        background-color: #40424B;
+        display: flex;
+        justify-content: space-between;
+        padding: 1% 1% 1% 1%;
+        color: #F3CC59;
+    }
+    header img {
+        max-width: 20%;
+        height: auto;
+        object-fit: contain;
+    }
+    nav {
+        display: flex;
+        align-items: center;
+        gap: 30%;
+        font-size: 200%;
+        max-width: 20%;
+        height: auto;
+    }
+    header button {
+        color: #F3CC59;
+        border-style: solid;
+        border-width: 3px;
+        border-radius: 30px;
+        border-color: #F3CC59;
+        font-size: 200%;
+        max-width: 20%;
+        height: auto;
+        padding: 0% 2% 0% 2%;
+    }
+</style>
--- flagfrenzy-sveltekit/src/routes/+page.svelte
+++ flagfrenzy-sveltekit/src/routes/+page.svelte
@@ -1,14 +1,5 @@
 <script>
     import { onMount } from 'svelte';
     import { writable } from 'svelte/store';
-  </script>
-  
-  <div>
-  <p>Hallo</p>
-  </div>
-  
-  <style>
-    h2 {
-        text-align: center;
-    }
-  </style>
+</script>
+
