bd3e56cd62d68c258f2f66c79fad22c2493e2e73
Leon Attila Sturm <attila@192.168.1.113>
2024-11-11 15:17:17 +0100

    working chllenges page/responsive UI/start of popup/change to tailwind

--- flagfrenzy-sveltekit/src/routes/challenges/+page.svelte
+++ flagfrenzy-sveltekit/src/routes/challenges/+page.svelte
@@ -21,7 +21,9 @@
 
 {#each Object.keys(challengesByCategory) as category}
   <h1 class="text-custom-200 text-2xl font-serif font-bold pt-4 pl-4">{category}</h1>
-  {#each challengesByCategory[category] as challenge}
-    <Card challenge={challenge} />
-  {/each}
+  <div class="place-items-center gap-3.5 px-8 py-4 mb-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+    {#each challengesByCategory[category] as challenge}
+      <Card challenge={challenge} />
+    {/each}
+  </div>
 {/each}
