a092c4b8ab9b576e93c92ecd4c8781ef1e19aed3
Leon Attila Sturm <attila@192.168.1.191>
2024-12-15 21:05:12 +0100

    Team page & Index Page mostly finished

--- flagfrenzy-sveltekit/src/routes/(protected)/+page.svelte
+++ flagfrenzy-sveltekit/src/routes/(protected)/+page.svelte
@@ -6,15 +6,16 @@
 <div class="flex items-center justify-center h-auto w-full">
     <img alt="The project logo" src={'/images/Slogan.png'} class="object-contain" />
 </div>
-
-<p class="mt-4 text-white text-center margin">
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent eu libero at nulla interdum bibendum. Quisque euismod, 
-    odio nec lacinia egestas, ligula eros vulputate nulla, sit amet luctus ipsum libero ac est. Nam auctor suscipit tortor, 
-    et tincidunt felis dictum at.
-</p>
-
-<p class="mt-2 text-white text-center">
-    Sed viverra ipsum ut magna consequat, nec ultrices augue cursus. Donec convallis, sapien nec volutpat placerat, 
-    velit ligula fringilla risus, id blandit nisi libero non purus. Etiam tempus ligula ac magna sagittis, 
-    a tempor erat aliquam.
-</p>
+<div class="my-8 mx-16">
+    <p class="text-white text-center text-xl">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent eu libero at nulla interdum bibendum. Quisque euismod, 
+        odio nec lacinia egestas, ligula eros vulputate nulla, sit amet luctus ipsum libero ac est. Nam auctor suscipit tortor, 
+        et tincidunt felis dictum at.
+    </p>
+    <br/>
+    <p class="text-white text-center text-xl">
+        Sed viverra ipsum ut magna consequat, nec ultrices augue cursus. Donec convallis, sapien nec volutpat placerat, 
+        velit ligula fringilla risus, id blandit nisi libero non purus. Etiam tempus ligula ac magna sagittis, 
+        a tempor erat aliquam.
+    </p>
+</div>
--- flagfrenzy-sveltekit/src/routes/(protected)/team/+page.svelte
+++ flagfrenzy-sveltekit/src/routes/(protected)/team/+page.svelte
@@ -77,78 +77,70 @@
 
 </script>
 
-
-
-<h1>Add New Team</h1>
-<form on:submit|preventDefault={addTeam}>
-    <label>
-        Team name:
-        <input type="text" bind:value={teamname} required>
-    </label>
-    <br>
-
-    <label>
-        Password:
-        <input type="text" bind:value={password} required>
-    </label>
-    <br>
-        <button type="submit">Join Team</button>
-
-    </form>
-  <div>
-  <br>
-  <h4>Teams</h4>
-      {#if teams}
-    <table class="styled-table">
-            <thead>
-                <tr>
-                    <th>ID</th>
-                    <th>Name</th>
-                </tr>
-            </thead>
-            <tbody>
-                {#each teams as team}
-                    <tr>
-                        <td>{team.ID}</td> 
-                        <td>{team.Teamname}</td>
-                
-                    </tr>
-                {/each}
-            </tbody>
-        </table>
-          {:else}
-    <p>Loading teams data...</p>
-  {/if}
-<br>
-  <h4>TeamMembers <h4>
-  {#if teamMembers}
-    <table class="styled-table">
-            <thead>
-                <tr>
-                    <th>Name</th>
-                    <th>Nickname</th>
-                </tr>
-            </thead>
-            <tbody>
-                {#each teamMembers as member}
-                    <tr>
-                        <td>{member.ID}</td> 
-                        <td>{member.Nickname}</td>
-                
-                    </tr>
-                {/each}
-            </tbody>
-        </table>
-          {:else}
-    <p>Loading Members data...</p>
-  {/if}
+<h1 class="text-custom-200 text-2xl font-serif font-bold pt-4 px-4">Create or Join Team</h1>
+<div class=" gap-7 px-4 mb-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2">
+  <form class="py-4 gap-3.5 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2">
+    <input class="bg-custom-100 border-2 border-custom-200 rounded-2xl px-2 py-1 text-xl w-full" type="text" placeholder="Team name" bind:value={teamname} required>
+    <input class="bg-custom-100 border-2 border-custom-200 rounded-2xl px-2 py-1 text-xl w-full" type="password" placeholder="Password" bind:value={password} required>
+  </form>
+  <div class="py-4 gap-3.5 flex">
+    <button class="text-custom-200 border-2 border-custom-200 rounded-2xl px-2 py-1 text-xl w-1/3 md:w-1/5 lg:w-1/6" type="submit" on:click={addTeam}>Create</button>
+    <button class="text-custom-200 border-2 border-custom-200 rounded-2xl px-2 py-1 text-xl w-1/3 md:w-1/5 lg:w-1/6" type="submit" on:click={joinTeam}>Join</button>
   </div>
-  
-  <style>
-    h2 {
-        text-align: center;
-    }
-    input {
-      background: black
-    }
-  </style>
+  <div class="">
+    <h1 class="text-custom-200 text-2xl font-serif font-bold pb-4">Teams</h1>
+    <div class="max-h-80 overflow-y-auto hide-scrollbar bg-custom-110 w-full px-4 rounded-2xl">
+      {#if teams}
+        <table class="styled-table w-full bg-custom-110 mt-4">
+          <thead class="text-custom-200 text-xl sticky top-0 bg-custom-110 border-b z-10 border-custom-200">
+            <tr>
+              <th>Name</th>
+              <th class="text-center">Members</th>
+            </tr>
+          </thead>
+          <tbody class="text-white text-sm">
+            {#each teams as team}
+              <tr class="border-b border-custom-100">
+                <td class="pt-2 align-top">{team.Teamname}</td>
+                <td class="pt-2 align-top text-center">{team.Members}/4</td>
+              </tr>
+            {/each}
+          </tbody>
+        </table>
+      {:else}
+        <p>Loading teams data...</p>
+      {/if}
+    </div>
+  </div>
+  <div class="">
+    <h1 class="text-custom-200 text-2xl font-serif font-bold pb-4">TeamMembers</h1>
+    <div class="max-h-80 overflow-y-auto hide-scrollbar bg-custom-110 w-2/3 px-4 py-4 rounded-2xl">
+      {#if teamMembers}
+        <table class="styled-table w-full">
+          <thead class="text-custom-200 text-xl border-b border-custom-200">
+            <tr>
+              <th>Name</th>
+              <th>Nickname</th>
+            </tr>
+          </thead>
+          <tbody class="text-white text-sm">
+            {#each teamMembers as member}
+              <tr class="border-b border-custom-100">
+                <td class="pt-2 align-top">{member.ID}</td> 
+                <td class="pt-2 align-top">{member.Nickname}</td>
+              </tr>
+            {/each}
+          </tbody>
+        </table>
+      {:else}
+        <p>Loading Members data...</p>
+      {/if}
+    </div>
+  </div>
+</div>
+
+<style>
+  .hide-scrollbar::-webkit-scrollbar {
+    display: none; /* Chrome, Safari, Edge */
+  }
+</style>
