f1c53e093aa5ad34bf36425c375d613bc02ce9c7
Leon Attila Sturm <attila@Leon-MacBook-Air.local>
2024-12-17 10:00:47 +0100

    Added Error Response in GUI

--- flagfrenzy-sveltekit/src/routes/(protected)/team/+page.svelte
+++ flagfrenzy-sveltekit/src/routes/(protected)/team/+page.svelte
@@ -1,13 +1,16 @@
 <script>
     import { onMount } from 'svelte';
     import { writable } from 'svelte/store';
-    let teamname = '';
-    let password = '';
-    let teams;
-    let teamMembers;
+
+    let teamname, password = '';
+    let teams, teamMembers;
+    let errorMessage, errorMessageTeams, errorMessageTeamMembers = '';
+
     export let data;
+
     async function joinTeam() {
       try {
+        errorMessage = '';
         const response = await fetch(`/api/user/team?id=${data.username}`, {
           method: "PUT",
           body: JSON.stringify({
@@ -16,18 +19,22 @@
           }),
           headers: {
             "Content-Type": "application/json; charset=UTF-8",
-          }
+          }  
         });
-  
-        console.log(response.text());
-  
+        if (!response.ok) {
+          throw new Error("Team konnte nicht beigetreten werden. Bitte überprüfe Teamname und Passwort.");
+        }
+
+        loadTeams();
+        loadTeamMember();
   
       } catch (error) {
-        console.error("Fehler beim Fetchen:", error);
+        errorMessage = error.message || "Es ist ein unbekannter Fehler aufgetreten.";
       } 
     } 
     async function addTeam() {
       try {
+        errorMessage = '';
         const response = await fetch("/api/teams", {
           method: "POST",
           body: JSON.stringify({
@@ -38,41 +45,50 @@
             "Content-Type": "application/json; charset=UTF-8",
           }
         });
+        if (!response.ok) {
+          throw new Error("Team konnte nicht erstellt werden. Bitte überprüfe den Teamname.");
+        }
         joinTeam()
-        console.log(response);
-  
-  
       } catch (error) {
-        console.error("Fehler beim Fetchen:", error);
+        errorMessage = error.message || "Es ist ein unbekannter Fehler aufgetreten.";
       } 
     } 
     
   async function loadTeams() {
     try {
+      errorMessageTeams, teams = '';
       const response = await fetch('/api/teams');
       if (!response.ok) throw new Error("Failed to load teams");
 
       teams = await response.json();
-      console.log(teams)
     } catch (err) {
-      error = err.message;
+      errorMessageTeams = err.message;
     }
   }
-    async function loadTeamMember() {
+  
+  async function loadTeamMember() {
     try {
+      errorMessageTeamMembers, teamMembers = '';
       const response = await fetch(`/api/teams/members?user_id=${data.username}`);
-      if (!response.ok) throw new Error("Failed to load teams");
+      if (!response.ok) throw new Error("Failed to load team members");
 
       teamMembers = await response.json();
-      console.log(teamMembers)
     } catch (err) {
-      error = err.message;
+      errorMessageTeamMembers = err.message;
     }
   }
-      onMount(() => {
-   loadTeams();
-   loadTeamMember();
+  
+  onMount(() => {
+    loadTeams();
+    loadTeamMember();
     
+    const interval = setInterval(() => {
+      loadTeams();
+      loadTeamMember();
+    }, 100000);
+
+    // Cleanup beim Verlassen der Komponente
+    return () => clearInterval(interval);
   });
 
 </script>
@@ -87,9 +103,12 @@
     <button class="text-custom-200 border-2 border-custom-200 rounded-2xl px-2 py-1 text-xl w-1/3 md:w-1/5 lg:w-1/6" type="submit" on:click={addTeam}>Create</button>
     <button class="text-custom-200 border-2 border-custom-200 rounded-2xl px-2 py-1 text-xl w-1/3 md:w-1/5 lg:w-1/6" type="submit" on:click={joinTeam}>Join</button>
   </div>
+  {#if errorMessage}
+    <p class=" text-Hard text-lg font-bold col-span-2">{errorMessage}</p>
+  {/if}
   <div class="">
     <h1 class="text-custom-200 text-2xl font-serif font-bold pb-4">Teams</h1>
-    <div class="max-h-80 overflow-y-auto hide-scrollbar bg-custom-110 w-full px-4 rounded-2xl">
+    <div class="h-80 max-h-80 overflow-y-auto hide-scrollbar bg-custom-110 w-full px-4 rounded-2xl">
       {#if teams}
         <table class="styled-table w-full bg-custom-110 mt-4">
           <thead class="text-custom-200 text-xl sticky top-0 bg-custom-110 border-b z-10 border-custom-200">
@@ -107,6 +126,8 @@
             {/each}
           </tbody>
         </table>
+      {:else if errorMessageTeams}
+        <p class=" text-Hard text-lg font-bold col-span-2">{errorMessageTeams}</p>
       {:else}
         <p>Loading teams data...</p>
       {/if}
@@ -132,6 +153,8 @@
             {/each}
           </tbody>
         </table>
+      {:else if errorMessageTeamMembers}
+        <p class=" text-Hard text-lg font-bold col-span-2">{errorMessageTeamMembers}</p>
       {:else}
         <p>Loading Members data...</p>
       {/if}
