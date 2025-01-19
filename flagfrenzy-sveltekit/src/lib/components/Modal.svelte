43e47bf63a88322d038ab3b0bbe9f89ded5a2cca
pintere6 <pintere@edu.htl-villach.at>
2025-01-19 09:20:03 +0100

    Rebase

--- flagfrenzy-sveltekit/src/lib/components/Modal.svelte
+++ flagfrenzy-sveltekit/src/lib/components/Modal.svelte
@@ -1,8 +1,6 @@
 <script>
   import { createEventDispatcher } from 'svelte';
-  import { onMount, onDestroy } from 'svelte';
-  import { tick } from 'svelte'; // Import tick to ensure DOM updates are complete
-
+  
   export let isOpen = false;
   export let data;
   let flagToSubmit;
@@ -73,13 +71,12 @@ async function checkChainCondition() {
         throw new Error("Hint wurde bereits benutzt.");
       }
 
-      hintUsed = true;
+      hintUsed = true; // Mark hint as used
       console.log("Hint used successfully.");
     } catch (error) {
       console.log(error.message || "Es ist ein Fehler aufgetreten.");
     }
   }
-
   async function startChallenge() {
     try {
       const response = await fetch("/api/user_made_challenges", {
@@ -102,16 +99,18 @@ async function checkChainCondition() {
   }
   async function submitChallenge() {
     try {
-      const response = await fetch(`/api/user_made_challenges?id=${user.ID}&challenge_id=${data.ID}`, {
-        method: "PUT",
+      const response = await fetch('/api/submit_flags', {
+        method: "POST",
         body: JSON.stringify({
+          id: user.username,
+          challenge_id: data.ID,
           Solved: 1
         }),
         headers: {
           "Content-Type": "application/json; charset=UTF-8",
         }
       });
-    
+
       if (!response.ok) {
         throw new Error("Challenge konnte nicht gestartet werden.");
       }
@@ -163,7 +162,8 @@ async function checkChainCondition() {
 </script>
 
 {#if isOpen}
-  <div class="fixed inset-0 bg-black bg-opacity-75 z-10" on:click={close} tabindex="0" role="button"></div>
+  <!-- svelte-ignore a11y-click-events-have-key-events -->
+  <div class="fixed inset-0 bg-black bg-opacity-75 z-10" on:click={close} tabindex="0" role="button" ></div>
   <div class="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 p-8 rounded-lg z-20 max-h-3/5 max-w-2xl w-11/12 bg-custom-110 shadow-button-{data.Difficulty} text-white">
     <button class="absolute top-2.5 right-2.5 text-xl cursor-pointer bg-none" on:click={close}>✖</button>
     <div class="flex justify-between">
@@ -171,24 +171,16 @@ async function checkChainCondition() {
       <h3 class="text-2xl mr-4">Difficulty: <span class="text-{data.Difficulty}">{data.Difficulty}</span></h3>
     </div>
     <h3 class="text-2xl">Description:</h3>
-    <p class="text-lg">{data.Description}</p>
+    <p class=" text-lg">{data.Description}</p>
     <h3 class="text-2xl">Hint:</h3>
     <p class="text-lg">{hints.Hint1}</p>
     <p class="text-lg">{hints.Hint2}</p>
     <p class="text-lg">{hints.Hint3}</p>
     <div class="flex justify-between items-center mt-auto pt-4 border-t border-custom-200">
       <input class="bg-custom-100 border-2 border-custom-200 rounded-full px-2 py-1 text-base" type="text" bind:value={flagToSubmit} placeholder="Enter Flag">
-      {#if canSubmit}
-        <button 
-            class="text-custom-200 border-2 border-custom-200 rounded-full px-2 py-1 text-base" 
-            on:click={submitChallenge}>
-            Submit
-        </button>
-      {:else}
-        <p>Du musst zuerst die Challenge davor abschließen!</p>
-      {/if}
+      <button class="text-custom-200 border-2 border-custom-200 rounded-full px-2 py-1 text-base" on:click={submitChallenge}>Submit</button>
       <button class="text-custom-200 border-2 border-custom-200 rounded-full px-2 py-1 text-base" on:click={startChallenge}>Start</button>
-      <button class="text-custom-200 border-2 border-custom-200 rounded-full px-2 py-1 text-base" on:click={hintCount} disabled={hintUsed}>{hintUsed ? "Hint Used" : "Get Hint"}</button>
+      <button class="text-custom-200 border-2 border-custom-200 rounded-full px-2 py-1 text-base" on:click={hintCount}  disabled={hintUsed}>{hintUsed ? "Hint Used" : "Get Hint"}</button>
     </div>
   </div>
 {/if}
