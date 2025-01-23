dc7c3ede4acb62cb12aed02e2428576542ddb17f
pintere6 <pintere@edu.htl-villach.at>
2025-01-23 12:14:46 +0100

    Update Points

--- flagfrenzy-sveltekit/src/routes/(protected)/api/user/points/+server.js
+++ flagfrenzy-sveltekit/src/routes/(protected)/api/user/points/+server.js
@@ -7,45 +7,23 @@ export async function PUT({ request }) {
     const { UserID, Points} = await request.json();
 
     try {
-        const response = await fetch(`${API_BASE_URL}/users/points/${user}`, {
-            method: "POST",
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                UserID: UserID,
-                points: Points
-            })
-        });
-
-        return jsonResponse(response, response.status);
-    } catch (error) {
-        return jsonResponse({ message: "An error occurred", error }, 500);
-    }
-}
-export async function PUT({ request, url }) {
-    const id = url.searchParams.get('id');
-    const { Nickname, Avatar} = await request.json();
-
-    try {
-        const response = await fetch(`${API_BASE_URL}/users/${id}`, {
+        const response = await fetch(`${API_BASE_URL}/users/points/${UserID}?points=${Points}`, {
             method: "PUT",
             headers: {
                 'Accept': 'application/json',
                 'Content-Type': 'application/json'
             },
             body: JSON.stringify({
-                Nickname: Nickname,
-                Avatar: Avatar
+                UserID: UserID
             })
         });
-        
+
         return jsonResponse(response, response.status);
     } catch (error) {
         return jsonResponse({ message: "An error occurred", error }, 500);
     }
 }
+
 function jsonResponse(data, status) {
     return new Response(JSON.stringify(data), {
         status: status,
