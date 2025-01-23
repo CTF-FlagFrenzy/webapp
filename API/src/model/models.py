d005ed188e41327a6eb0e34a8d2840f488b8846f
pintere <pintere@edu.htl-villach.at>
2025-01-23 08:04:49 +0100

    Updated Static Flag

--- API/src/model/models.py
+++ API/src/model/models.py
@@ -77,7 +77,7 @@ class Challenge(Base):
     Hint1 = Column(Text(400), nullable=True, default=None)
     Hint2 = Column(Text(400), nullable=True, default=None)
     Hint3 = Column(Text(400), nullable=True, default=None)
-
+    IsStatic = Column(Integer, nullable=False, default=0)
 
     made_by_users = relationship("UserMadeChallenge", back_populates="challenge")
     flag_submissions = relationship("FlagSubmission", back_populates="challenge")
@@ -125,13 +125,13 @@ class SharedFlagSubmission(Base):
     team = relationship("Team", foreign_keys=[team_id], back_populates="shared_flag_submissions")
     original_team = relationship("Team", foreign_keys=[original_team_id])
 
-class StaticFlag(Base):
-    __tablename__ = 'static_flags'
+# class StaticFlag(Base):
+#     __tablename__ = 'static_flags'
 
-    id = Column(Integer, primary_key=True, index=True)
-    flag = Column(String(255), unique=True, nullable=False)
-    challenge_id = Column(Integer, ForeignKey("Challenges.ID"), index=True, nullable=False)
-    created_at = Column(DateTime(timezone=True), server_default=func.now())
+#     id = Column(Integer, primary_key=True, index=True)
+#     flag = Column(String(255), unique=True, nullable=False)
+#     challenge_id = Column(Integer, ForeignKey("Challenges.ID"), index=True, nullable=False)
+#     created_at = Column(DateTime(timezone=True), server_default=func.now())
 
-    challenge = relationship("Challenge", back_populates="static_flags")
+#     challenge = relationship("Challenge", back_populates="static_flags")
 
