3797b7c1ed2447357cd1ba598f2480a879aea385
pintere <pintere@edu.htl-villach.at>
2025-02-16 16:13:43 +0100

    Began with Admin page

--- flagfrenzy-sveltekit/src/routes/(protected)/admin/+page.svelte
+++ flagfrenzy-sveltekit/src/routes/(protected)/admin/+page.svelte
@@ -1,6 +1,11 @@
-<a href="http://localhost:8000/docs" target="_blank">Fast API</a>
 <script>
     let allFlags;
+    import Graph from '$lib/components/graph.svelte';
+
+let teamPoints, interval;
+let errorMessageTeams = '';
+  export let data;
+
     
  async function loadFlags() {
       try {
@@ -21,7 +26,41 @@
     }
   }
 
+async function loadGraphValue() {
+  try {
+
+    errorMessageTeams, teamPoints = '';
+    const response = await fetch(`/api/teampoints/admin`);
+
+    if (!response.ok) throw new Error("Failed to load values");
+    teamPoints = '';
+    teamPoints = await response.json();
+    console.log(teamPoints)
+  } catch (err) {
+    errorMessageTeams = err.message;
+  }
+}
+onMount(async () => {
+  await loadGraphValue();
+  interval = setInterval((loadTeams, loadGraphValue), 300000); // Refresh every 60 seconds
+  return () => {
+    clearInterval(interval); // Clean up interval when component is destroyed
+  };
+});
+
 </script>
+
+<div class="px-8 pt-8">
+{#if teamPoints}
+  <div class="px-32 hidden sm:block">
+    <Graph data={teamPoints}/>
+  </div>
+  <p class="sm:hidden text-center text-white">Der Graph wird nur auf größeren Bildschirmen angezeigt.</p>
+{:else}
+  <p class="hidden sm:block">Loading graph data...</p>
+  <p class="sm:hidden text-center text-white">Der Graph wird nur auf größeren Bildschirmen angezeigt.</p>
+{/if}
+</div>
 <style>
     a {
         color: white;
